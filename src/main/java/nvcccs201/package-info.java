3a5e0dabb4466fdda102df5c08e04ef082a335b8
D1 <D1@192.168.86.92>
2019-11-10 13:30:36 -0500

    MavenGitHub Test

---
+++ src/main/java/nvcccs201/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * 
+ * Write a one-sentence summary of your class here.
+ * Follow it with additional details about its purpose
+ * and how to use it.
+ *
+ * @author D1
+ * @version Nov 9, 2019
+ *
+ */
+package nvcccs201;
